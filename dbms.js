64e5b800f0cd961ea04d8d15bb13303275b9bf34
CJ Apel <cjapel@liveramp.com>
2017-05-05 10:33:20 -0700

    commit

--- dbms.js
+++ dbms.js
@@ -2,20 +2,26 @@
 
   ROOT: () => SELECTION(PROJECTION())
   PROJECTION: ([]columns, record) => []columns
-  SELECTION: ([(predicates)..], record) => record | null
+  SELECTION: ([(predicates)..], scanner) => record | null
   SCAN: () => record
 
 */
-var recordIndex = 0;
+var TEST_NAME = "https://www.cia.gov/library/readingroom/document/cia-rdp80-00810a002400700005-2";
+var {equal} = require('./util.js');
 var {makeIterator} = require('./test_data');
-var {columns} = require('./test_data');
+var {columns} = require('./util');
 
-// records.forEach(record => {
-//   // record.applyProjection(userProjection).applySelection(userSelection);
-// });
+var selector = (predicate, scanner) => ({
+  next () {
+    const row = scanner.next().value;
+    if (row === undefined) {return {done: true}}
+    if (predicate(row)) {
+      return row;
+    }
+  }
+});
 
-var scanner = makeIterator()[Symbol.iterator]();
-
-console.log(scanner.next());
-console.log(scanner.next());
-console.log(scanner.next());
+const x = selector(equal("url", TEST_NAME), makeIterator()[Symbol.iterator]());
+console.log(x.next());
+console.log(x.next());
+console.log(x.next());
