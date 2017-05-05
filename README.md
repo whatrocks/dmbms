4b8d3a9b0779d7f267e7c791233f91e13c8d1328
Charlie Harrington <charlie.harrington@lendup.com>
2017-05-05 10:00:51 -0700

    add sample and logo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dmbms
 Database Matthews Band Management System
+
+<img src="./dmbms.png" />
---
+++ sample.js
@@ -0,0 +1,35 @@
+// projection(
+
+//   select(
+//     // series of predicates to choose the rows
+//     equals(
+//       colName,
+//       value
+//     ),
+//     equals(
+//       colName,
+//       value
+//     ),
+//   )
+//   ,
+//   // array of output columns
+//   [
+//     colName,
+//     count(colName),
+//     distinct(colName)
+//   ]
+// )
+
+
+
+// var select = function(predicates) {
+
+//   next() => {
+
+//     predicates(
+
+//     )
+
+//   }
+
+// }
