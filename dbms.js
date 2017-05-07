4e4002811f0136ceed4b0f565f1c57685e86c469
CJ Apel <cjapel@liveramp.com>
2017-05-07 13:03:25 -0700

    quick and dirty join merge

--- dbms.js
+++ dbms.js
@@ -9,6 +9,7 @@
 var TEST_NAME = "https://www.cia.gov/library/readingroom/document/cia-rdp80-00810a002400700005-2";
 var {equal} = require('./util.js');
 var {makeIterator} = require('./test_data');
+var {rows} = require('./test_data');
 var {columns} = require('./util');
 
 var selector = (predicate, scanner) => ({
@@ -21,7 +22,42 @@ var selector = (predicate, scanner) => ({
   }
 });
 
-const x = selector(equal("url", TEST_NAME), makeIterator()[Symbol.iterator]());
+function hasher(table, property) {
+  let idx = columns.indexOf(property);
+  return table.reduce((hashMap, record) => {
+    let colValue = record[idx];
+    let previousValues = hashMap[colValue] || [];
+    hashMap[colValue] = [...previousValues, record];
+    return hashMap;
+
+    return hashMap;
+  } ,{})
+}
+
+// console.log(hasher(rows, "url"))
+
+function joinRecords (record1, record2, columnJoinedOn) {
+  let idx = columns.indexOf(columnJoinedOn);
+  const record1WithoutTheColumn = record1.slice(0, idx).concat(record1.slice(idx+1));
+  const record2WithoutTheColumn = record1.slice(0, idx).concat(record1.slice(idx+1));
+  return [columnJoinedOn, record1WithoutTheColumn, record2WithoutTheColumn];
+}
+
+function joiner(hashMap, property, scanner, theta=x=>true) {
+  const record = scanner.next().value;
+  let idx = columns.indexOf(property);
+  const colValue = record[idx];
+  const hashedRecords = hashMap[colValue];
+  const numberOfReturnedJoinedRows = 0;
+  return function next () {
+    if (numberOfReturnedJoinedRows >= hashedRecords.length) {
+      return {done: true}
+    }
+    return joinRecords(record, hashedRecords[numberOfReturnedJoinedRows++]);
+  }
+}
+
+// const x = selector(equal("url", TEST_NAME), makeIterator()[Symbol.iterator]());
 // for (let record of makeIterator()) {
 //   console.log(record);
 // }
